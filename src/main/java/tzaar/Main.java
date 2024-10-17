f8c2b76ce926ed52ff4b907e7ce94f66102d751d
László Mócsy <laszlomocsy.dev@gmail.com>
2024-10-17 16:38:27 +0200

    Remove unnecessary `GameFrame`

--- src/main/java/tzaar/Main.java
+++ src/main/java/tzaar/Main.java
@@ -1,11 +1,30 @@
 package tzaar;
 
-import tzaar.gui.MainFrame;
+import tzaar.component.Board;
+import tzaar.gui.GamePanel;
+import tzaar.gui.SidePanel;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Initialize and start the game
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.startGame();
+        // Initialize the frame
+        JFrame frame = new JFrame("Tzaar game");
+
+        // Initialize the SidePanel
+        SidePanel sidePanel = new SidePanel();
+        frame.add(sidePanel, BorderLayout.EAST);
+
+        // Initialize the GamePanel
+        GamePanel gamePanel = new GamePanel(new Board());
+        frame.add(gamePanel, BorderLayout.CENTER);
+
+        // Make the frame visible
+        frame.setSize(740, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
