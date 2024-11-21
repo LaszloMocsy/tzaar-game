5ce67a3d72e5321407260fbe7a35a16e87b34a9e
LaszloMocsy <LaszloMocsy@users.noreply.github.com>
2024-11-21 15:41:56 +0000

    deploy: 70b808a4a05d079033fb10053de65f9d52111220

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dev.laszlomocsy.tzaar"},{"l":"dev.laszlomocsy.tzaar.gui"},{"l":"dev.laszlomocsy.tzaar.logic.board"},{"l":"dev.laszlomocsy.tzaar.logic.figure"},{"l":"dev.laszlomocsy.tzaar.utils"}];updateSearchResults();
